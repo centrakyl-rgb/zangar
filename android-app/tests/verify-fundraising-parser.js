a41c5197b75ddabe1b57893289fd61d24ed6ebc9
centrakyl-rgb <centrakyl@gmail.com>
2026-08-24 04:00:04 +0300

    Test fundraising amount updates dynamically

--- android-app/tests/verify-fundraising-parser.js
+++ android-app/tests/verify-fundraising-parser.js
@@ -1,17 +1,19 @@
-ПРОВЕРКА ПРОЙДЕНА: собрано 44 760 ₽, цель 350 000 ₽, прогресс 13%
 const fs = require('fs');
 
 const java = fs.readFileSync('app/src/main/java/center/akyl/app/MainActivity.java', 'utf8');
+function parseAmount(value) {
 const page = `
 <div class="main-page__need-item">
   <div class="main-page__need-heading">Уже собрали</div>
-  <div class="main-page__need-subheading main-page__need-subheading-need">44&nbsp;760&nbsp;₽</div>
+  <div class="main-page__need-subheading main-page__need-subheading-need">${value.replace(' ', '&nbsp;')}&nbsp;₽</div>
 </div>`;
 
 const normalizedPage = page.replaceAll('&nbsp;', ' ').replaceAll('&#8381;', '₽');
 const match = normalizedPage.match(/main-page__need-heading[^>]*>\s*Уже собрали\s*<\/[^>]+>.*?main-page__need-subheading[^>]*>\s*([0-9][0-9\s\u00a0.,]{0,20})\s*₽/isu);
-const amount = Number((match?.[1] || '').replace(/\s/g, ''));
+return Number((match?.[1] || '').replace(/\s/g, ''));
+}
 
-if (amount !== 44760) throw new Error(`Парсер вернул ${amount}, ожидалось 44760`);
-if (!java.includes('collected.equals(goal)') || !java.includes('collected = "44760"')) throw new Error('Нет защиты от ошибочных 100%');
-console.log('ПРОВЕРКА ПРОЙДЕНА: собрано 44 760 ₽, цель 350 000 ₽, прогресс 13%');
+if (parseAmount('44 760') !== 44760) throw new Error('Не разобрана старая сумма');
+if (parseAmount('45 145') !== 45145) throw new Error('Не разобрана новая сумма');
+if (java.includes('collected = "44760"')) throw new Error('В приложении осталась фиксированная сумма');
+console.log('ПРОВЕРКА ПРОЙДЕНА: сумма обновляется с 44 760 ₽ до 45 145 ₽');
